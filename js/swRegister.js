94fb7686b7e2e681c695f86a78b51f7c88efe61c
emiliero <emilie.rosnas@hotmail.com>
2022-05-10 14:41:20 +0200

    Project code

---
+++ js/swRegister.js
@@ -0,0 +1,49 @@
+export default async () => {
+    if (!("serviceWorker" in navigator)) {
+        return;
+    }
+
+    const swRegistration = await navigator.serviceWorker.register("sw.js", {scope: ""});
+    let serviceWorker;
+
+    if (swRegistration.installing) {
+        console.log("Resolved on installing: ", serviceWorker);
+        serviceWorker = swRegistration.installing;
+    } else if (swRegistration.waiting) {
+        console.log("Resolved on waiting: ", serviceWorker);
+        serviceWorker = swRegistration.waiting;
+    } else if (swRegistration.active) {
+        console.log("Resolved on active: ", serviceWorker);
+        serviceWorker = swRegistration.active;
+    }
+
+    serviceWorker.addEventListener("statechenge", (e) => {
+        console.log(e.target.state);
+    });
+
+    swRegistration.addEventListener("updatefound", () => {
+        swRegister.installing.addEventListener("statechange", e => {
+            console.log("New service worker state: ", e.target.state);
+        });
+        console.log("New service worker found!", swRegistration);
+    });
+    // An extra event that is fired when the service worker contrling this page changes
+    // through the self.skipWaiting()
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+        console.log("Controller Changed");
+    });
+
+    navigator.serviceWorker,addEventListener("message"), e => {
+        const clientId = e.data.clientId;
+        const message = event.data.message;
+        console.log("From client: ", clientId, message);
+    }
+
+    if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller.postMessage("hello");
+    }
+
+    setInterval(() => {
+        swRegistration.update();
+    }, 1000 * 5);
+}
